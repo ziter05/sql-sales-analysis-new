f4fa0803feb0368d031339532680058ea113dc56
ziter05 <55512250+ziter05@users.noreply.github.com>
2026-06-29 12:51:49 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,31 @@
+/* sum sales */
+
 SELECT SUM(p.price * o.quantity) AS total_sales
 FROM orders o
 JOIN products p ON o.product_id = p.product_id;
+
+/* sales per customer */
+SELECT c.name, SUM(p.price * o.quantity) AS total_spent
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN products p ON o.product_id = p.product_id
+GROUP BY c.name;
+
+/* best customer */
+  
+SELECT c.name, SUM(p.price * o.quantity) AS total_spent
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN products p ON o.product_id = p.product_id
+GROUP BY c.name
+ORDER BY total_spent DESC
+LIMIT 1;
+
+/* best product */
+  
+SELECT p.name, SUM(o.quantity) AS total_quantity
+FROM orders o
+JOIN products p ON o.product_id = p.product_id
+GROUP BY p.name
+ORDER BY total_quantity DESC
+LIMIT 1;
